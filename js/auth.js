ea63c4dd7eb292cbe150e9de47873366e3e75330
XMyUsername <bl.a.zcx01@gmail.com>
2025-04-20 09:23:39 +0000

    Implementación inicial del sitio web SYREM 2

--- js/auth.js
+++ js/auth.js
@@ -100,7 +100,6 @@ document.addEventListener('DOMContentLoaded', function() {
         verifyCodeBtn.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Verificando...';
         
         const codeValue = document.getElementById('verificationCodeInput').value.trim();
-
         // Ahora usa codeValue en lugar de code
         fetch('https://xblazcx.pythonanywhere.com/api/verify-code', {
             method: 'POST',
